26afa9433a5d528a49afc7cba87c6eba277aa4bc
tvatt <github@vattima.com>
2026-02-04 22:37:26 -0500

    Commit and pushing local desktop work

--- src/main/java/com/vattima/lego/inventory/service/config/LegoDataServiceRestAutoConfiguration.java
+++ src/main/java/com/vattima/lego/inventory/service/config/LegoDataServiceRestAutoConfiguration.java
@@ -4,11 +4,8 @@ import com.vattima.lego.inventory.service.controller.ItemController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @Configuration
 @ConditionalOnClass(ItemController.class)
